44f319db13f8151bfd78239f79ba21898a5d0e4a
Wang CHEN <solid.chen9@gmail.com>
2022-01-12 10:46:46 +0100

    update gitignore

--- components/Author.tsx
+++ components/Author.tsx
@@ -1,11 +1,23 @@
-import React from 'react'
+import React from 'react';
+import Image from 'next/image';
 
-const Author = () => {
-    return (
-        <div>
-            <h1>Author</h1>
-        </div>
-    )
-}
+const Author = ({ author }) => {
+  return (
+    <div className="text-center mt-20 mb-8 p-10 relative rounded-lg bg-black bg-opacity-10">
+      <div className="overflow-hidden">
+        <Image
+          alt={author.name}
+          unoptimized
+          height={100}
+          width={100}
+          src={author.photo.url}
+          className="align-middle object-center object-cover overflow-hidden rounded-full"
+        />
+      </div>
+      <h3 className="text-white my-4 text-xl font-bold">{author.name}</h3>
+      <p className="text-white text-lg">{author.bio}</p>
+    </div>
+  );
+};
 
-export default Author
+export default Author;
--- components/CommentsForm.tsx
+++ components/CommentsForm.tsx
@@ -1,11 +1,19 @@
-import React from 'react'
+import React, { useState, useEffect, useRef } from 'react';
 
-const CommentsForm = () => {
-    return (
-        <div>
-           <h1>CommentForm</h1> 
-        </div>
-    )
-}
+const CommentsForm = ({ slug }) => {
+  const [error, setError] = useState(false);
+  const [localStorage, setLocalStorage] = useState(null);
+  const [showSuccessMessage, setShowSuccessMessage] = useState(false);
+  const commeentEl = useRef();
+  const nameEl = useRef();
+  const emailEl = useRef();
+  const storeDataEl = useRef();
 
-export default CommentsForm
+  return (
+    <div className="bg-white shadow-lg roudned-lg p-8 pb-12 mb-8">
+      <h1>CommentForm</h1>
+    </div>
+  );
+};
+
+export default CommentsForm;
